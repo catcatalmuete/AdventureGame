77943bebcbcaac0b371017550b27ea0c26cc8682
WAngieZhao <wz1342@nyu.edu>
2021-11-05 09:20:52 -0400

    Update Comments

--- Adventure Game/Assets/Script/PublicVars.cs
+++ Adventure Game/Assets/Script/PublicVars.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PublicVars : MonoBehaviour
 {
+    //updated after each roll, will be updated after buffs/debuffs are applied
     public static int diceVal = 0;
     public static bool instantDeath = true;
+    //sum all buffs/debuffs the player has
     public static int diceRollModifier = 0;
     // Start is called before the first frame update
     void Start()
