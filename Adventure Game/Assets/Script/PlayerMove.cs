61f6fb6cb60c143549f40901224fe73712899229
elijah-whittle <edw2290@nyu.edu>
2021-11-12 07:59:21 -0500

    add music, title screen, and win screen

--- Adventure Game/Assets/Script/PlayerMove.cs
+++ Adventure Game/Assets/Script/PlayerMove.cs
@@ -20,11 +20,15 @@ public class PlayerMove : MonoBehaviour
     }
 
     public Room[] rooms;
+    List<Room> roomsToFinish;
+    private Room currentRoom;
 
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
         mainCam = Camera.main;
+        currentRoom = rooms[(int)Color.Blue];
+        roomsToFinish = new List<Room>(rooms);
     }
 
     void Update()
@@ -51,44 +55,66 @@ public class PlayerMove : MonoBehaviour
 
     public void Teleport(int diceroll)
     {
-        // diceroll = 4;
-        // recaled on 4 rooms
-        if (diceroll <= 2){
-            //teleport purple / bad
-            //transform.position = new Vector3(-50, 1, 50);
-            _navMeshAgent.Warp(rooms[(int)Color.Purple].spawnPos.position);
-            print("purple");
+        // remove current room from rooms to finish if it's finished
+        if (currentRoom.complete)
+        {
+            roomsToFinish.Remove(currentRoom);
+        }
+        // if no rooms are left to finish, the player wins!
+        if (roomsToFinish.Count == 0)
+        {
+            FindObjectOfType<TransitionManager>().LoadScene("Win");
             return;
         }
-        else if(diceroll <= 4){
-            //teleport green / normal
-            _navMeshAgent.Warp(rooms[(int)Color.Green].spawnPos.position);
-            print("green");
-            return;
+        // determine the available rooms to visit
+        int roomSep = 12 / roomsToFinish.Count; // divide rolls among remaining rooms
+        int roomVal = (diceroll-1) / roomSep;   // set val according to roll
+        for (int i = 0; i < roomsToFinish.Count; ++i)
+        {
+            if (roomVal <= i)
+            {
+                currentRoom = roomsToFinish[i];
+                _navMeshAgent.Warp(roomsToFinish[i].spawnPos.position);
+            }
         }
-        else if(diceroll <= 6){
-            //teleport blue / initial
-            //transform.position = new Vector3(-0, 1, 0);
-            _navMeshAgent.Warp(rooms[(int)Color.Blue].spawnPos.position);
-            print("blue");
-        }
-        else if(diceroll <= 8){
-            //teleport orange / good
-            //transform.position = new Vector3(-0, 1, 0);
-            _navMeshAgent.Warp(rooms[(int)Color.Orange].spawnPos.position);
-            print("orange");
-        }
-        else if(diceroll <= 9){
-            //teleport orange / good
-            //transform.position = new Vector3(-0, 1, 0);
-            _navMeshAgent.Warp(rooms[(int)Color.Red].spawnPos.position);
-            print("red");
-        }
-        else {
-            //teleport orange / good
-            //transform.position = new Vector3(-0, 1, 0);
-            _navMeshAgent.Warp(rooms[(int)Color.White].spawnPos.position);
-            print("white");
-        }
+        //// diceroll = 4;
+        //// recaled on 4 rooms
+        //if (diceroll <= 2){
+        //    //teleport purple / bad
+        //    //transform.position = new Vector3(-50, 1, 50);
+        //    _navMeshAgent.Warp(rooms[(int)Color.Purple].spawnPos.position);
+        //    print("purple");
+        //    return;
+        //}
+        //else if(diceroll <= 4){
+        //    //teleport green / normal
+        //    _navMeshAgent.Warp(rooms[(int)Color.Green].spawnPos.position);
+        //    print("green");
+        //    return;
+        //}
+        //else if(diceroll <= 6){
+        //    //teleport blue / initial
+        //    //transform.position = new Vector3(-0, 1, 0);
+        //    _navMeshAgent.Warp(rooms[(int)Color.Blue].spawnPos.position);
+        //    print("blue");
+        //}
+        //else if(diceroll <= 8){
+        //    //teleport orange / good
+        //    //transform.position = new Vector3(-0, 1, 0);
+        //    _navMeshAgent.Warp(rooms[(int)Color.Orange].spawnPos.position);
+        //    print("orange");
+        //}
+        //else if(diceroll <= 9){
+        //    //teleport orange / good
+        //    //transform.position = new Vector3(-0, 1, 0);
+        //    _navMeshAgent.Warp(rooms[(int)Color.Red].spawnPos.position);
+        //    print("red");
+        //}
+        //else {
+        //    //teleport orange / good
+        //    //transform.position = new Vector3(-0, 1, 0);
+        //    _navMeshAgent.Warp(rooms[(int)Color.White].spawnPos.position);
+        //    print("white");
+        //}
     }
 }
